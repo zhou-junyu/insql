edceeec3fd52f05800f682ba16b0dc5a2229d063
rainrcn <rainrcn@qq.com>
2019-02-12 16:21:22 +0800

    ..

--- src/Insql/Resolvers/InsqlSection.cs
+++ src/Insql/Resolvers/InsqlSection.cs
@@ -6,18 +6,7 @@ namespace Insql.Resolvers
 {
     public class InsqlSection
     {
-        /// <summary>
-        /// {IdName},
-        /// {IdName}.{ServerName},
-        /// {IdName}.{ServerName}.{ServerVersion}
-        /// </summary>
         public string Id { get; }
-
-        public string IdName { get; }
-
-        public string ServerName { get; }
-
-        public int? ServerVersion { get; }
 
         public List<IInsqlSectionElement> Elements { get; }
 
@@ -28,23 +17,7 @@ namespace Insql.Resolvers
                 throw new ArgumentNullException(nameof(id));
             }
 
-            var splitId = id.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-
             this.Id = id;
-
-            if (splitId.Length > 0)
-            {
-                this.IdName = splitId[0];
-            }
-            if (splitId.Length > 1)
-            {
-                this.ServerName = splitId[1];
-            }
-            if (splitId.Length > 2)
-            {
-                this.ServerVersion = Convert.ToInt32(splitId[2]);
-            }
-
             this.Elements = new List<IInsqlSectionElement>();
         }
 
