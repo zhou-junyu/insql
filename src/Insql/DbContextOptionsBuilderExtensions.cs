38bb3cb93ff7d48f8affcd3017a80a445781bdf1
rainrcn <rainrcn@qq.com>
2019-04-03 17:11:14 +0800

    ..

--- src/Insql/DbContextOptionsBuilderExtensions.cs
+++ src/Insql/DbContextOptionsBuilderExtensions.cs
@@ -5,6 +5,13 @@ namespace Insql
 {
     public static partial class DbContextOptionsBuilderExtensions
     {
+        public static DbContextOptionsBuilder CommandTimeout(this DbContextOptionsBuilder builder, int? commandTimeout)
+        {
+            builder.Options.CommandTimeout = commandTimeout;
+
+            return builder;
+        }
+
         public static DbContextOptionsBuilder UseResolver<TContext>(this DbContextOptionsBuilder builder) where TContext : class
         {
             builder.Options.Resolver = builder.Options.ServiceProvider.GetRequiredService<IInsqlResolver<TContext>>();
