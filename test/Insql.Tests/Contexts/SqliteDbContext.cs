9931e4ec36dd2df7444114b5b87a6d458f49f0cf
rainrcn <rainrcn@qq.com>
2019-04-05 20:06:52 +0800

    ..

---
+++ test/Insql.Tests/Contexts/SqliteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insql.Tests.Contexts
+{
+    public class SqliteDbContext : DbContext
+    {
+        public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
