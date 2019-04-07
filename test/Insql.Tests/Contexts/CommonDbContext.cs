37d9422b7124b2abbe3bdcb3fdf5971dc9acd41e
rainrcn <rainrcn@qq.com>
2019-04-07 13:40:03 +0800

    ..

---
+++ test/Insql.Tests/Contexts/CommonDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Insql.Tests.Contexts
+{
+    public class CommonDbContext<T> : DbContext where T : class
+    {
+        public CommonDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseResolver<T>();
+        }
+    }
+}
