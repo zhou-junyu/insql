c9b6a68c6f51b5ef85c8155b87023a90069c3382
rainrcn <rainrcn@qq.com>
2019-05-03 17:56:44 +0800

    修改.net 框架中查询类型异常问题

--- src/Insql/Mappers/Internal/InsqlModel.cs
+++ src/Insql/Mappers/Internal/InsqlModel.cs
@@ -10,199 +10,221 @@ using System.Reflection;
 
 namespace Insql.Mappers
 {
-    internal class InsqlModel : IInsqlModel
-    {
-        private readonly Dictionary<Type, IInsqlEntityMap> entityMaps = new Dictionary<Type, IInsqlEntityMap>();
+   internal class InsqlModel : IInsqlModel
+   {
+      private readonly Dictionary<Type, IInsqlEntityMap> entityMaps = new Dictionary<Type, IInsqlEntityMap>();
 
-        public IEnumerable<IInsqlEntityMap> Maps => this.entityMaps.Values;
+      public IEnumerable<IInsqlEntityMap> Maps => this.entityMaps.Values;
 
-        public InsqlModel(IOptions<InsqlModelOptions> options, IInsqlDescriptorLoader descriptorLoader, IInsqlModelMapper entityMapper)
-        {
-            var optionsValue = options.Value;
+      public InsqlModel(IOptions<InsqlModelOptions> options, IInsqlDescriptorLoader descriptorLoader, IInsqlModelMapper entityMapper)
+      {
+         var optionsValue = options.Value;
 
-            if (optionsValue.IncludeAnnotationMaps)
+         if (optionsValue.IncludeAnnotationMaps)
+         {
+            this.LoadAnnotationEntityMaps(optionsValue.IncludeAnnotationMapsAssemblies);
+         }
+
+         if (optionsValue.IncludeFluentMaps)
+         {
+            this.LoadFluentEntityMaps(optionsValue.IncludeFluentMapsAssemblies);
+         }
+
+         if (optionsValue.IncludeXmlMaps)
+         {
+            this.LoadXmlEntityMaps(descriptorLoader);
+         }
+
+         entityMapper.Mapping(this.entityMaps.Values);
+      }
+
+      public IInsqlEntityMap FindMap(Type entityType)
+      {
+         if (entityType == null)
+         {
+            throw new ArgumentNullException(nameof(entityType));
+         }
+
+         if (this.entityMaps.TryGetValue(entityType, out IInsqlEntityMap entityMap))
+         {
+            return entityMap;
+         }
+
+         return null;
+      }
+
+      private void LoadAnnotationEntityMaps(IEnumerable<Assembly> assemblies)
+      {
+         if (assemblies == null)
+         {
+            assemblies = AppDomain.CurrentDomain.GetAssemblies();
+         }
+
+         assemblies = assemblies.Where(assembly => !assembly.IsDynamic && !assembly.ReflectionOnly);
+
+         var resultMaps = assemblies.SelectMany(assembly =>
+         {
+            IEnumerable<TypeInfo> definedTypes;
+
+            try
             {
-                this.LoadAnnotationEntityMaps(optionsValue.IncludeAnnotationMapsAssemblies);
+               definedTypes = assembly.DefinedTypes;
+            }
+            catch
+            {
+               definedTypes = new List<TypeInfo>();
             }
 
-            if (optionsValue.IncludeFluentMaps)
-            {
-                this.LoadFluentEntityMaps(optionsValue.IncludeFluentMapsAssemblies);
-            }
+            return definedTypes.Where(type =>
+                   type.IsPublic &&
+                   type.IsClass &&
+                   !type.IsAbstract &&
+                   !type.IsGenericType &&
+                   type.GetCustomAttribute(typeof(TableAttribute), true) != null)
+               .Select(type => this.CreateAnnotationEntityMap(type));
+         }).ToDictionary(item => item.EntityType, item => item);
 
-            if (optionsValue.IncludeXmlMaps)
-            {
-                this.LoadXmlEntityMaps(descriptorLoader);
-            }
+         foreach (var itemMap in resultMaps)
+         {
+            this.entityMaps[itemMap.Key] = itemMap.Value;
+         }
+      }
 
-            entityMapper.Mapping(this.entityMaps.Values);
-        }
+      private IInsqlEntityMap CreateAnnotationEntityMap(Type entityType)
+      {
+         TableAttribute tableAttribute = (TableAttribute)entityType.GetCustomAttribute(typeof(TableAttribute), true);
 
-        public IInsqlEntityMap FindMap(Type entityType)
-        {
-            if (entityType == null)
-            {
-                throw new ArgumentNullException(nameof(entityType));
-            }
+         IInsqlEntityMap resultMap = new InsqlEntityMap(entityType, tableAttribute.Name, tableAttribute.Schema);
 
-            if (this.entityMaps.TryGetValue(entityType, out IInsqlEntityMap entityMap))
-            {
-                return entityMap;
-            }
+         var columnMaps = entityType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Select(propInfo =>
+          {
+             ColumnAttribute columnAttribute = (ColumnAttribute)propInfo.GetCustomAttribute(typeof(ColumnAttribute), true);
+             KeyAttribute keyAttribute = (KeyAttribute)propInfo.GetCustomAttribute(typeof(KeyAttribute), true);
+             NotMappedAttribute notMappedAttribute = (NotMappedAttribute)propInfo.GetCustomAttribute(typeof(NotMappedAttribute), true);
+             EditableAttribute editableAttribute = (EditableAttribute)propInfo.GetCustomAttribute(typeof(EditableAttribute), true);
+             DatabaseGeneratedAttribute databaseGeneratedAttribute = (DatabaseGeneratedAttribute)propInfo.GetCustomAttribute(typeof(DatabaseGeneratedAttribute), true);
 
-            return null;
-        }
+             InsqlPropertyMap propertyMap = new InsqlPropertyMap(propInfo, columnAttribute?.Name);
 
-        private void LoadAnnotationEntityMaps(IEnumerable<Assembly> assemblies)
-        {
-            if (assemblies == null)
-            {
-                assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            }
-
-            assemblies = assemblies.Where(assembly => !assembly.IsDynamic && !assembly.ReflectionOnly);
-
-            var resultMaps = assemblies.SelectMany(assembly =>
-            {
-                return assembly.DefinedTypes.Where(type =>
-                    type.IsPublic &&
-                    type.IsClass &&
-                    !type.IsAbstract &&
-                    !type.IsGenericType &&
-                    type.GetCustomAttribute(typeof(TableAttribute), true) != null)
-                .Select(type => this.CreateAnnotationEntityMap(type));
-            }).ToDictionary(item => item.EntityType, item => item);
-
-            foreach (var itemMap in resultMaps)
-            {
-                this.entityMaps[itemMap.Key] = itemMap.Value;
-            }
-        }
-
-        private IInsqlEntityMap CreateAnnotationEntityMap(Type entityType)
-        {
-            TableAttribute tableAttribute = (TableAttribute)entityType.GetCustomAttribute(typeof(TableAttribute), true);
-
-            IInsqlEntityMap resultMap = new InsqlEntityMap(entityType, tableAttribute.Name, tableAttribute.Schema);
-
-            var columnMaps = entityType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Select(propInfo =>
+             if (keyAttribute != null)
              {
-                 ColumnAttribute columnAttribute = (ColumnAttribute)propInfo.GetCustomAttribute(typeof(ColumnAttribute), true);
-                 KeyAttribute keyAttribute = (KeyAttribute)propInfo.GetCustomAttribute(typeof(KeyAttribute), true);
-                 NotMappedAttribute notMappedAttribute = (NotMappedAttribute)propInfo.GetCustomAttribute(typeof(NotMappedAttribute), true);
-                 EditableAttribute editableAttribute = (EditableAttribute)propInfo.GetCustomAttribute(typeof(EditableAttribute), true);
-                 DatabaseGeneratedAttribute databaseGeneratedAttribute = (DatabaseGeneratedAttribute)propInfo.GetCustomAttribute(typeof(DatabaseGeneratedAttribute), true);
-
-                 InsqlPropertyMap propertyMap = new InsqlPropertyMap(propInfo, columnAttribute?.Name);
-
-                 if (keyAttribute != null)
-                 {
-                     propertyMap.IsKey = true;
-                 }
-                 if (notMappedAttribute != null)
-                 {
-                     propertyMap.IsIgnored = true;
-                 }
-                 if (databaseGeneratedAttribute != null)
-                 {
-                     if (databaseGeneratedAttribute.DatabaseGeneratedOption == DatabaseGeneratedOption.Identity)
-                     {
-                         propertyMap.IsIdentity = true;
-                     }
-                 }
-                 if (editableAttribute != null && !editableAttribute.AllowEdit)
-                 {
-                     propertyMap.IsReadonly = true;
-                 }
-
-                 return propertyMap;
-             });
-
-            foreach (var columnMap in columnMaps)
-            {
-                resultMap.Properties.Add(columnMap);
-            }
-
-            InsqlEntityValidator.Instance.Validate(resultMap);
-
-            return resultMap;
-        }
-
-        private void LoadFluentEntityMaps(IEnumerable<Assembly> assemblies)
-        {
-            var baseType = typeof(InsqlEntityBuilder);
-
-            if (assemblies == null)
-            {
-                assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            }
-
-            assemblies = assemblies.Where(assembly => !assembly.IsDynamic && !assembly.ReflectionOnly);
-
-            var resultMaps = assemblies.SelectMany(assembly =>
-            {
-                return assembly.DefinedTypes.Where(type =>
-                    type.IsPublic &&
-                    type.IsClass &&
-                    !type.IsAbstract &&
-                    !type.IsGenericType &&
-                    baseType != type &&
-                    baseType.IsAssignableFrom(type))
-                .Select(type => (InsqlEntityBuilder)Activator.CreateInstance(type))
-                .Select(builder => builder.Build());
-            }).ToDictionary(item => item.EntityType, item => item);
-
-            foreach (var itemMap in resultMaps)
-            {
-                this.entityMaps[itemMap.Key] = itemMap.Value;
-            }
-        }
-
-        private void LoadXmlEntityMaps(IInsqlDescriptorLoader descriptorLoader)
-        {
-            var mapSections = new Dictionary<Type, IInsqlMapSection>();
-
-            var insqlDescriptors = descriptorLoader.Load().Values;
-
-            foreach (var descriptor in insqlDescriptors)
-            {
-                foreach (var map in descriptor.Maps.Values)
+                propertyMap.IsKey = true;
+             }
+             if (notMappedAttribute != null)
+             {
+                propertyMap.IsIgnored = true;
+             }
+             if (databaseGeneratedAttribute != null)
+             {
+                if (databaseGeneratedAttribute.DatabaseGeneratedOption == DatabaseGeneratedOption.Identity)
                 {
-                    mapSections[map.Type] = map;
+                   propertyMap.IsIdentity = true;
                 }
+             }
+             if (editableAttribute != null && !editableAttribute.AllowEdit)
+             {
+                propertyMap.IsReadonly = true;
+             }
+
+             return propertyMap;
+          });
+
+         foreach (var columnMap in columnMaps)
+         {
+            resultMap.Properties.Add(columnMap);
+         }
+
+         InsqlEntityValidator.Instance.Validate(resultMap);
+
+         return resultMap;
+      }
+
+      private void LoadFluentEntityMaps(IEnumerable<Assembly> assemblies)
+      {
+         var baseType = typeof(InsqlEntityBuilder);
+
+         if (assemblies == null)
+         {
+            assemblies = AppDomain.CurrentDomain.GetAssemblies();
+         }
+
+         assemblies = assemblies.Where(assembly => !assembly.IsDynamic && !assembly.ReflectionOnly);
+
+         var resultMaps = assemblies.SelectMany(assembly =>
+         {
+            IEnumerable<TypeInfo> definedTypes;
+
+            try
+            {
+               definedTypes = assembly.DefinedTypes;
+            }
+            catch
+            {
+               definedTypes = new List<TypeInfo>();
             }
 
-            var resultMaps = mapSections.Values.Select(mapSection =>
+            return definedTypes.Where(type =>
+                   type.IsPublic &&
+                   type.IsClass &&
+                   !type.IsAbstract &&
+                   !type.IsGenericType &&
+                   baseType != type &&
+                   baseType.IsAssignableFrom(type))
+               .Select(type => (InsqlEntityBuilder)Activator.CreateInstance(type))
+               .Select(builder => builder.Build());
+         }).ToDictionary(item => item.EntityType, item => item);
+
+         foreach (var itemMap in resultMaps)
+         {
+            this.entityMaps[itemMap.Key] = itemMap.Value;
+         }
+      }
+
+      private void LoadXmlEntityMaps(IInsqlDescriptorLoader descriptorLoader)
+      {
+         var mapSections = new Dictionary<Type, IInsqlMapSection>();
+
+         var insqlDescriptors = descriptorLoader.Load().Values;
+
+         foreach (var descriptor in insqlDescriptors)
+         {
+            foreach (var map in descriptor.Maps.Values)
             {
-                var entityMap = string.IsNullOrWhiteSpace(mapSection.Table) ?
-                    new InsqlEntityMap(mapSection.Type) :
-                    new InsqlEntityMap(mapSection.Type, mapSection.Table, mapSection.Schema);
-
-                foreach (var mapElement in mapSection.Elements.Values)
-                {
-                    var propertyInfo = mapSection.Type.GetProperty(mapElement.Property);
-
-                    if (propertyInfo == null)
-                    {
-                        throw new Exception($"insql entity type : {mapSection.Type} `{mapElement.Property}` property is not exist!");
-                    }
-
-                    entityMap.Properties.Add(new InsqlPropertyMap(propertyInfo, mapElement.Name)
-                    {
-                        IsIdentity = mapElement.Identity,
-                        IsKey = mapElement.ElementType == InsqlMapElementType.Key
-                    });
-                }
-
-                InsqlEntityValidator.Instance.Validate(entityMap);
-
-                return entityMap;
-            }).ToDictionary(item => item.EntityType, item => (IInsqlEntityMap)item);
-
-            foreach (var itemMap in resultMaps)
-            {
-                this.entityMaps[itemMap.Key] = itemMap.Value;
+               mapSections[map.Type] = map;
             }
-        }
-    }
+         }
+
+         var resultMaps = mapSections.Values.Select(mapSection =>
+         {
+            var entityMap = string.IsNullOrWhiteSpace(mapSection.Table) ?
+                   new InsqlEntityMap(mapSection.Type) :
+                   new InsqlEntityMap(mapSection.Type, mapSection.Table, mapSection.Schema);
+
+            foreach (var mapElement in mapSection.Elements.Values)
+            {
+               var propertyInfo = mapSection.Type.GetProperty(mapElement.Property);
+
+               if (propertyInfo == null)
+               {
+                  throw new Exception($"insql entity type : {mapSection.Type} `{mapElement.Property}` property is not exist!");
+               }
+
+               entityMap.Properties.Add(new InsqlPropertyMap(propertyInfo, mapElement.Name)
+               {
+                  IsIdentity = mapElement.Identity,
+                  IsKey = mapElement.ElementType == InsqlMapElementType.Key
+               });
+            }
+
+            InsqlEntityValidator.Instance.Validate(entityMap);
+
+            return entityMap;
+         }).ToDictionary(item => item.EntityType, item => (IInsqlEntityMap)item);
+
+         foreach (var itemMap in resultMaps)
+         {
+            this.entityMaps[itemMap.Key] = itemMap.Value;
+         }
+      }
+   }
 }
