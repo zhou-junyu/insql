86c03e05b0e0c3e0f17d1c07bbb29501eef916cd
rainrcn <rainrcn@qq.com>
2019-07-12 14:55:05 +0800

    同一个Descriptor Provider 中同一类型（作用域）中的Section,Map不能重复

--- src/Insql/Providers/Internal/InsqlDescriptorLoader.cs
+++ src/Insql/Providers/Internal/InsqlDescriptorLoader.cs
@@ -4,54 +4,90 @@ using System.Collections.Generic;
 
 namespace Insql.Providers
 {
-    internal class InsqlDescriptorLoader : IInsqlDescriptorLoader
-    {
-        private readonly IEnumerable<IInsqlDescriptorProvider> descriptorProviders;
+   internal class InsqlDescriptorLoader : IInsqlDescriptorLoader
+   {
+      private readonly IEnumerable<IInsqlDescriptorProvider> descriptorProviders;
 
-        private readonly Dictionary<Type, InsqlDescriptor> insqlDescriptors;
+      private readonly Dictionary<Type, InsqlDescriptor> insqlDescriptors;
 
-        public InsqlDescriptorLoader(IEnumerable<IInsqlDescriptorProvider> descriptorProviders)
-        {
-            this.descriptorProviders = descriptorProviders;
+      public InsqlDescriptorLoader(IEnumerable<IInsqlDescriptorProvider> descriptorProviders)
+      {
+         this.descriptorProviders = descriptorProviders;
 
-            this.insqlDescriptors = this.LoadInsqlDescriptors();
-        }
+         this.insqlDescriptors = this.LoadInsqlDescriptors();
+      }
 
-        public IDictionary<Type, InsqlDescriptor> Load()
-        {
-            return this.insqlDescriptors;
-        }
+      public IDictionary<Type, InsqlDescriptor> Load()
+      {
+         return this.insqlDescriptors;
+      }
 
-        private Dictionary<Type, InsqlDescriptor> LoadInsqlDescriptors()
-        {
-            var resultDescriptors = new Dictionary<Type, InsqlDescriptor>();
+      private Dictionary<Type, InsqlDescriptor> LoadInsqlDescriptors()
+      {
+         var allDescriptors = new Dictionary<Type, InsqlDescriptor>();
 
-            foreach (var provider in this.descriptorProviders)
+         foreach (var provider in this.descriptorProviders)
+         {
+            //同一个提供者不支持同一类型（作用域）内的Section,Map重名
+            var providerDescriptors = new Dictionary<Type, InsqlDescriptor>();
+
+            foreach (var descriptor in provider.GetDescriptors())
             {
-                foreach (var descriptor in provider.GetDescriptors())
-                {
-                    if (resultDescriptors.TryGetValue(descriptor.Type, out InsqlDescriptor insqlDescriptor))
-                    {
-                        //sections
-                        foreach (var section in descriptor.Sections)
-                        {
-                            insqlDescriptor.Sections[section.Key] = section.Value;
-                        }
+               if (providerDescriptors.TryGetValue(descriptor.Type, out InsqlDescriptor insqlDescriptor))
+               {
+                  //sections
+                  foreach (var section in descriptor.Sections)
+                  {
+                     if (insqlDescriptor.Sections.ContainsKey(section.Key))
+                     {
+                        throw new Exception($"{descriptor.Type} section: {section.Key} already exists!");
+                     }
 
-                        //maps
-                        foreach (var map in descriptor.Maps)
-                        {
-                            insqlDescriptor.Maps[map.Key] = map.Value;
-                        }
-                    }
-                    else
-                    {
-                        resultDescriptors.Add(descriptor.Type, descriptor);
-                    }
-                }
+                     insqlDescriptor.Sections[section.Key] = section.Value;
+                  }
+
+                  //maps
+                  foreach (var map in descriptor.Maps)
+                  {
+                     if (insqlDescriptor.Maps.ContainsKey(map.Key))
+                     {
+                        throw new Exception($"{descriptor.Type} map: {map.Key} already exists!");
+                     }
+
+                     insqlDescriptor.Maps[map.Key] = map.Value;
+                  }
+               }
+               else
+               {
+                  providerDescriptors.Add(descriptor.Type, descriptor);
+               }
             }
 
-            return resultDescriptors;
-        }
-    }
+            //merge provider
+            foreach (var descriptor in providerDescriptors.Values)
+            {
+               if (allDescriptors.TryGetValue(descriptor.Type, out InsqlDescriptor insqlDescriptor))
+               {
+                  //sections
+                  foreach (var section in descriptor.Sections)
+                  {
+                     insqlDescriptor.Sections[section.Key] = section.Value;
+                  }
+
+                  //maps
+                  foreach (var map in descriptor.Maps)
+                  {
+                     insqlDescriptor.Maps[map.Key] = map.Value;
+                  }
+               }
+               else
+               {
+                  allDescriptors.Add(descriptor.Type, descriptor);
+               }
+            }
+         }
+
+         return allDescriptors;
+      }
+   }
 }
